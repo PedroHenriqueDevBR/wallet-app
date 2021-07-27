79074dcc003267a920dc37878f8ab9e9604b2d5f
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-07-27 18:08:20 -0300

    style: create nav sidebar style component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavSidebarComponent } from './components/nav-sidebar/nav-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavSidebarComponent,
   ],
   imports: [
     BrowserModule,
