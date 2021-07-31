f0433e69bd712e2cadf8cabcbbc0ac65b20fca15
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-07-30 23:35:54 -0300

    feat: create quick transfer component

--- frontend/src/app/components/quick-transfer/quick-transfer.component.ts
+++ frontend/src/app/components/quick-transfer/quick-transfer.component.ts
@@ -7,10 +7,24 @@ import { QuickTransferService } from 'src/app/services/quick-transfer.service';
   styleUrls: ['./quick-transfer.component.css']
 })
 export class QuickTransferComponent implements OnInit {
+  currencies: Array<string> = ['USD', 'BRL']
+  quickTransfers: Array<string> = ['Jane', 'Esther', 'Ronald', 'Cameron', 'Robert','Darlene', 'Coby'];
+  selectedQuickTranfer: number | undefined = undefined;
+  selectedCurrency: string = '';
 
-  constructor(
-    private quickTransferService: QuickTransferService
-  ) { }
+  constructor() { }
+
+  changeCurrency(event: any) : void {
+    this.selectedCurrency = this.currencies[event.value];
+  }
+
+  changeQuickTransfer(index: number) : void {
+    if (this.selectedQuickTranfer == index) {
+      this.selectedQuickTranfer = undefined;
+      return;
+    }
+    this.selectedQuickTranfer = index;
+  }
 
   ngOnInit(): void {
   }
